6f0906cce20d8f59971ca6a848dccfe35f09dc6a
Riley Kong <kongriley@gmail.com>
2021-04-03 17:47:02 -0700

    Add nuxt framework

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=PT+Serif&family=Oxygen&display=swap');
+html {
+  font-family: "PT Serif", serif;
+}
+.header {
+  font-family: "Oxygen", sans-serif;
+}
+</style>
